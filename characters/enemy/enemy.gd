24a1e51d8a09270ead76ffb06729a2c829ef1018
AtlasWongy <atlasyjyj@gmail.com>
2023-10-13 16:49:34 +0800

    patrol_adjustment

--- characters/enemy/enemy.gd
+++ characters/enemy/enemy.gd
@@ -4,15 +4,22 @@ extends CharacterBody2D
 @onready var player_detection = $PlayerDetection
 @onready var animation_player = $Weapon/WeaponSprite/AnimationPlayer
 @onready var cool_down = $CoolDown
+@onready var patrol = $Patrol
 
 const SPEED = 50.0
 const ACCELERATION = 800.0
 const FRICTION = 1000.0
 
+signal turn_on_patrol_signal
+signal turn_off_patrol_signal
+
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var direction = 1.0
 var facing_right: bool = true
 var can_attack: bool = true
+
+func _ready():
+	patrol.change_state_signal.connect(change_state)
 	
 func _physics_process(delta):
 	if not is_on_floor():
@@ -28,9 +35,11 @@ func handle_acceleration(delta):
 func detect_the_player():
 	if player_detection.is_colliding():
 		var collider = player_detection.get_collider()
-		if collider == null:
+		print("I knocked into: ", player_detection.get_collider())
+		if collider == null or !collider.is_in_group('player'):
 			return
 		if collider.is_in_group('player') and can_attack:
+			emit_signal("turn_off_patrol_signal")
 			animation_player.play("swing")
 			velocity.x = 0.0
 			can_attack = false
